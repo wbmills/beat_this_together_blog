29ab322f979e7586c62aea56d706ddc46b8d50f3
jq4u <93946961+jq4u@users.noreply.github.com>
2023-04-01 18:00:43 +0100

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -134,8 +134,6 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-"""
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-"""
