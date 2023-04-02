4cfe70e307bffbf0fd2a6c3c37bd0bc111560bbb
willmills345 <will.b.mills@outlook.com>
2023-04-02 14:21:25 +0100

    it doesn't mess up on phone view now :)

--- blog/settings.py
+++ blog/settings.py
@@ -77,11 +77,23 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ZX52AsPeT0Q771LDeYi5',
+        'HOST': 'containers-us-west-192.railway.app',
+        'PORT': '5630',
     }
 }
 
