eb4008429c0b0e270f2d8acbd826d90b54a09aaa
willmills345 <will.b.mills@outlook.com>
2023-10-12 12:26:03 +0100

    Revert "temp removed recipe but frank is still up"
    This reverts commit 11a26fd98be2ada5e64d243644587b3e97471b7e.

--- meal_maker_app/urls.py
+++ meal_maker_app/urls.py
@@ -4,10 +4,10 @@ from django.views.generic.base import RedirectView
 from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
-    #path('', views.home, name='home'),
-    #path('password', views.pw, name='pw'),
-    #path('<str:clicked>/', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('password', views.pw, name='pw'),
+    path('<str:clicked>/', views.home, name='home'),
     path('frank', views.frankContent, name="frankContent"),
-    #path('js-tutorial', views.js_tutorial, name='js_tutorial'),
-    #path('create', views.createRecipe, name='create'),
+    path('js-tutorial', views.js_tutorial, name='js_tutorial'),
+    path('create', views.createRecipe, name='create'),
 ]
