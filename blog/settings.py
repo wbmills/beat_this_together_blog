75fe0a97fc25f43b41ad3e404ece359a98542332
willmills345 <will.b.mills@outlook.com>
2023-10-13 17:13:48 +0100

    static files change

--- blog/settings.py
+++ blog/settings.py
@@ -136,9 +136,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles/'
+STATIC_ROOT = BASE_DIR / 'static'
 
-#STATICFILES_DIRS = [BASE_DIR / 'static',]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
