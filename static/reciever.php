d5e6d9a709e0bb34283ca49987252b106a14fcbf
willmills345 <will.b.mills@outlook.com>
2024-01-27 22:51:54 +0000

    add php

--- static/reciever.php
+++ static/reciever.php
@@ -1,11 +1,14 @@
 <?php
-// receiver.php
+// get_x.php
 
-if (isset($_POST['data'])) {
-    $receivedData = $_POST['data'];
-    // Process the data as needed
-    echo "Received data: $receivedData";
+if (isset($_GET['x'])) {
+    // Retrieve the value of the "x" parameter
+    $xValue = $_GET['x'];
+
+    // Return the number 12
+    echo "The value of x is: $xValue, and the result is 12";
 } else {
-    echo "No data received.";
+    // If "x" parameter is not provided
+    echo "Please provide a value for the 'x' parameter.";
 }
 ?>
